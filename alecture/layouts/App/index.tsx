ef298a006e22c67547e26768efbbf68cf4680c0a
SEOYUN KIM <nado0903@gmail.com>
2022-04-25 23:39:11 +0900

    Modify: loadable 코드스플리팅

--- alecture/layouts/App/index.tsx
+++ alecture/layouts/App/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import loadable from '@loadable/component';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
-import LogIn from '@pages/LogIn';
-import SignUp from '@pages/SignUp';
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
+
 //import Workspace from '@layouts/Workspace';
 
 const App = () => {
